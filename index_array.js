f42f351c03a96ac74721fa50e4b648adccd4c016
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-06-04 22:19:50 +0530

    Add files via upload

---
+++ index_array.js
@@ -0,0 +1,5 @@
+
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[2] = "Pineapple";
+document.getElementById("demo").innerHTML = fruits;
---
+++ index_fun.js
@@ -0,0 +1,12 @@
+<p id="demo"></p>
+function callboy();
+
+let name  = "Priyanka";
+
+let msg = " Hello" +  "are you " + name ;
+
+console.log(msg);
+
+callboy()
+
+document.getElementById("demo").innerHTML = callboy;
