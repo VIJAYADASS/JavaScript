41e8bd9e8cbf0752bc6f59a5370f7e6bf68f5190
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-06-04 22:21:32 +0530

    Add files via upload

---
+++ dt.js
@@ -0,0 +1,13 @@
+let firstName = "Laptop";    //string 
+
+let age = 14; // integers
+
+let isYoung = true ;  // boolean
+
+
+let lastName = "undefined";  //undefined
+
+console.log(firstName);
+console.log(age);
+console.log(isYoung);
+console.log(lastName);
