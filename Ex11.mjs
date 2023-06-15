2f250436fc42af8b7a7feec1021f4a5bc67c7b81
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-06-15 14:35:51 +0530

    Using import and export files

---
+++ Ex11.mjs
@@ -0,0 +1,15 @@
+// ex 11 using loop in last ex 10 list
+
+ export function exercise4() 
+  {
+        // Exercise 4 here
+        var list = [ "wicketkeeping ", "batting", "captiancy",  "finisher","Cool"];
+        for (var i = 0; i < 5; i++)
+     {
+        console.log(list[i]);
+     } 
+       console.log("Exercise 4");
+       
+  }
+ // module.exports=exercise4; 
+    
---
+++ Im.mjs
@@ -0,0 +1,9 @@
+import { exercise1 } from './Ex01.mjs';
+import { exercise2 } from './Ex07.mjs';
+import { exercise3 } from './Ex09.mjs';
+import { exercise4 } from './Ex11.mjs';
+
+exercise1();
+exercise2();
+exercise3();
+exercise4();
