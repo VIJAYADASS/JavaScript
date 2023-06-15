e04a015076d8ac7201aa500bfaf2115282a18cd3
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-06-15 14:44:19 +0530

    using the keyword is: "require".

---
+++ Ex11.js
@@ -0,0 +1,15 @@
+// ex 11 using loop in last ex 10 list
+
+ export function exercise2() 
+  {
+        // Exercise 2 here
+        var list = [ "wicketkeeping ", "batting", "captiancy",  "finisher","Cool"];
+        for (var i = 0; i < 5; i++)
+     {
+        console.log(list[i]);
+     } 
+       console.log("Exercise 2");
+       
+  }
+  
+    
---
+++ Rq_main.js
@@ -0,0 +1,18 @@
+ // Require file or Calling function
+
+const exercises = {
+  exercise1: require('./Ex01'),
+  exercise2: require('./Ex11'),
+  exercise3: require('./Ex09'),
+  exercise4: require('./Ex07'),
+  
+};
+
+// Calling Exercises
+exercises.exercise1();
+console.log("**********");
+exercises.exercise2();
+console.log("**********");
+exercises.exercise3();
+console.log("************");
+exercises.exercise4();
