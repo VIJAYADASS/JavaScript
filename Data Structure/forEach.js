d46d6e0c79532d2fffb386e72d3127d9da40962c
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-14 17:27:34 +0530

    Data Structure in ForEach

---
+++ Data Structure/forEach.js
@@ -0,0 +1,7 @@
+const Movies = ["Ironman", "Batman", "Thor", "Spiderman", "Hulk", "Thanos", "Captain America"]
+
+function isMarvel(Yes){
+    console.log(` Endgame Movie cast is ${Yes}`)
+}
+
+Movies.forEach(isMarvel);
