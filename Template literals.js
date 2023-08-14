4a58ce78fbb202fc35b1da183166fdb879b3e372
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-14 13:38:19 +0530

    Add the another example of template literals

---
+++ Template literals.js
@@ -0,0 +1,13 @@
+//Template literals
+let temp = 23
+let place = 'Edinburgh'
+
+// console.log("Today it's " + temp + " degrees C here in " + place)
+console.log(`Today it's ${temp} degrees C here in ${place}`) // using the backstick and curly bracket and dolor sign
+
+//Example
+
+let Temp  = 32
+let place1 = 'Tamilnadu'
+
+console.log(`Today it's ${Temp} degree C here in ${place1}`)// use dolor sign means identify the object
