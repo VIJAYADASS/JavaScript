4079ee56e684cc10e0bd1856f7b75843f5c20e4b
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-14 17:29:07 +0530

    Update forEach.js

--- Data Structure/forEach.js
+++ Data Structure/forEach.js
@@ -1,6 +1,6 @@
 const Movies = ["Ironman", "Batman", "Thor", "Spiderman", "Hulk", "Thanos", "Captain America"]
 
-function isMarvel(Yes){
+function isMarvel(Yes, index){
     console.log(` Endgame Movie cast is ${Yes}`)
 }
 
