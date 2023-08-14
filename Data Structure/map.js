916f8536b22f29a93a769fbfaef910cc8e8be058
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-14 17:51:18 +0530

    map method

---
+++ Data Structure/map.js
@@ -0,0 +1,6 @@
+const Movies = ["Ironman", "Batman", "Thor", "Spiderman", "Hulk", "Thanos", "Captain America"]
+
+EndgameMovieCast = Movies.map(function (Yes){
+    return `${Yes}`
+});
+console.log(EndgameMovieCast);
