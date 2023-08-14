1c6a38f4cf48eeeaa0a920e0ec261126aa9548b7
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-14 18:10:46 +0530

    Update map.js

--- Data Structure/map.js
+++ Data Structure/map.js
@@ -4,3 +4,20 @@ EndgameMovieCast = Movies.map(function (Yes){
     return `${Yes}`
 });
 console.log(EndgameMovieCast);
+
+// Task 
+// Now refactor your code to use the .map method. 
+// You should no longer need to use the .push() 
+// method or declare an empty array.
+
+const numbers = [ 10,2, 3, 1, 0, 40, 56, 99, 78];
+
+const filteredNumbers = numbers.map(number => {
+  if (number >= 3 && number <= 40) {
+    return number;
+  }
+});
+
+console.log(filteredNumbers.filter(number => number !== undefined));
+
+
