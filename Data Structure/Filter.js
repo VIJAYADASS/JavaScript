456900e562111465c3ca2835c3d753f960c381f0
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-14 18:04:02 +0530

    remove a num greater and less than user given

--- Data Structure/Filter.js
+++ Data Structure/Filter.js
@@ -6,3 +6,14 @@ ValidatedNumber = Number.filter(function(num){
 })
 
 console.log(ValidatedNumber);
+
+// Task 
+// Use the filter method to:
+//        - Remove any number greater than 40.
+//        - Remove any number less than 3.
+
+const kmWalked = [ 10,2, 3, 1, 0, 40, 56, 99, 78 ]
+
+const filteredNumbers = kmWalked.filter(number => number <= 40 && number >= 3);
+
+console.log( filteredNumbers);
