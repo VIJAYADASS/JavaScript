af55218c23c3dd1c5555d548892d02a497dbf12d
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-15 20:08:04 +0530

    .

--- Data Structure/Spread operator.js
+++ Data Structure/Spread operator.js
@@ -1,31 +1,34 @@
-const kellysPets = ['dog', 'cat', 'fish']
-const jessiesPets = ['snake', 'rat', 'piglet']
-let allPets = [...kellysPets, ...jessiesPets]
+// const kellysPets = ['dog', 'cat', 'fish']
+// const jessiesPets = ['snake', 'rat', 'piglet']
+// let allPets = [...kellysPets, ...jessiesPets]
 
-// allPets = [...allPets, 'goat']
+// // allPets = [...allPets, 'goat']
 
-console.log(allPets)
+// console.log(allPets)
 
 
-const kellysPetsNames = {dog: 'Benny', cat: 'Whisky', fish: 'Nemo'}
-const jessiesPetsNames = {snake: 'Silver', rat: 'Barnie', piglet: 'Dave'}
-const allPetsNames = {...kellysPetsNames, ...jessiesPetsNames}
-console.log(allPetsNames)
+// const kellysPetsNames = {dog: 'Benny', cat: 'Whisky', fish: 'Nemo'}
+// const jessiesPetsNames = {snake: 'Silver', rat: 'Barnie', piglet: 'Dave'}
+// const allPetsNames = {...kellysPetsNames, ...jessiesPetsNames}
+// console.log(allPetsNames)
 
-// Task:
-//     1. Use the spread operator to combine the two objects 
-//        of Oscar winners into a single object just called 
-//        'oscarWinners'.
-//     2. Add the 2015 winner, which was called 'Spotlight', 
-//        to 'oscarWinners'. (Be careful, you might need to change 
-//        the const to a let!) 
+// // Task:
+// //     1. Use the spread operator to combine the two objects 
+// //        of Oscar winners into a single object just called 
+// //        'oscarWinners'.
+// //     2. Add the 2015 winner, which was called 'Spotlight', 
+// //        to 'oscarWinners'. (Be careful, you might need to change 
+// //        the const to a let!) 
 
-const oscarWinnersOne = {2018: 'Green Book', 2017: 'The Shape of Water', 2016: 'Moonlight'}
-const oscarWinnersTwo = {2021: 'Nomadland', 2020: 'Coda', 2019: 'Parasite'}
-let OscarWinners = {...oscarWinnersOne, ...oscarWinnersTwo} 
+// const oscarWinnersOne = {2018: 'Green Book', 2017: 'The Shape of Water', 2016: 'Moonlight'}
+// const oscarWinnersTwo = {2021: 'Nomadland', 2020: 'Coda', 2019: 'Parasite'}
+// let OscarWinners = {...oscarWinnersOne, ...oscarWinnersTwo} 
 
-OscarWinners = {...OscarWinners, 2015: 'Spotlight'} // Add the onemore OscarWinners 
+// OscarWinners = {...OscarWinners, 2015: 'Spotlight'} // Add the onemore OscarWinners 
 
-console.log(OscarWinners)
+// console.log(OscarWinners)
 
- 
+ function count(...food){
+    console.log(food.length);
+ }
+ count("a", "e",null)
