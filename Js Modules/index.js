fd693f52c7a32c0fc9472194af64dedc68189765
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-16 11:23:00 +0530

    Modules Js

---
+++ Js Modules/index.js
@@ -0,0 +1,5 @@
+import {addTwo, addThree, addFour} from "./mathOperations";
+
+addTwo(3, 4);
+addThree(3, 4, 5);
+addFour(5);
---
+++ Js Modules/mathOperations.js
@@ -0,0 +1,17 @@
+export function addTwo(a, b) {
+    console.log(a + b);
+}
+
+export function addThree(a, b, c) {
+    console.log(a + b + c);
+}
+
+export function addFour(a){
+    console.log(a);
+}
+
+/*
+Task:
+Create a third function. Export it from this file
+and import it into the index.js file.
+*/
